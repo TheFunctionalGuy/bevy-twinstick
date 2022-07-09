733dfed149eab5fbd1d429aeac16c508fc7cf436
Julian V <jvesper95@gmail.com>
2022-07-09 20:10:42 +0200

    Add player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+// Constants
+const PLAYER_COLOR: Color = Color::BLUE;
+
 // Components
 #[derive(Component)]
 struct Player;
@@ -8,12 +11,20 @@ struct Player;
 struct Enemy;
 
 #[derive(Component)]
-struct Health;
+struct Health(i32);
 
+// TODO:
+// 1. Spawn player (blue tile)
+// 2. Player movement
+// 3. Monster spawner (red tiles)
+// 4. First weapon (rectangles) NOTE: What about projectiles?
+// 5. Weapon switching
+// 6. Shooting + Aiming
 fn main() {
     App::new()
         // Startup Systems
         .add_startup_system(setup_camera)
+        .add_startup_system(spawn_player)
         // Systems
         .add_system(hello_world_system)
         // Plugins
@@ -24,6 +35,23 @@ fn main() {
 // Systems
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: PLAYER_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Player)
+        .insert(Health(5));
 }
 
 // fn spawn_player(commmands: Commands) {

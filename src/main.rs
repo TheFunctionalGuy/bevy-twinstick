44168b0394d3fbe5141d514eafb30908c228d2e9
Julian V <jvesper95@gmail.com>
2022-07-09 19:49:01 +0200

    Add default plugins and camera

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use bevy::prelude::*;
 
+// Components
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Enemy;
+
+#[derive(Component)]
+struct Health;
+
 fn main() {
-    App::new().add_system(hello_world_system).run();
+    App::new()
+        // Startup Systems
+        .add_startup_system(setup_camera)
+        // Systems
+        .add_system(hello_world_system)
+        // Plugins
+        .add_plugins(DefaultPlugins)
+        .run();
 }
+
+// Systems
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+// fn spawn_player(commmands: Commands) {
+//     commmands.s
+// }
 
 fn hello_world_system() {
     println!("Hello, World!");

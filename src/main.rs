2edc2e6f408bda2ede1ff1e8d6b727b3e0b1e829
Julian V <jvesper95@gmail.com>
2022-07-09 20:27:55 +0200

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ struct Enemy;
 struct Health(i32);
 
 // TODO:
-// 1. Spawn player (blue tile)
-// 2. Player movement
-// 3. Monster spawner (red tiles)
-// 4. First weapon (rectangles) NOTE: What about projectiles?
-// 5. Weapon switching
-// 6. Shooting + Aiming
+// 1. Player movement
+// 2. Monster spawner (red tiles)
+// 3. First weapon (rectangles) NOTE: What about projectiles?
+// 4. Weapon switching
+// 5. Shooting + Aiming
 fn main() {
     App::new()
         // Startup Systems
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_player)
         // Systems
+        .add_system(player_movement)
         // Plugins
         .add_plugins(DefaultPlugins)
         .add_plugin(LogDiagnosticsPlugin::default())
@@ -54,4 +54,21 @@ fn spawn_player(mut commands: Commands) {
         })
         .insert(Player)
         .insert(Health(5));
+}
+
+fn player_movement(keys: Res<Input<KeyCode>>, mut q: Query<&mut Transform, With<Player>>) {
+    let mut player_transform = q.single_mut();
+
+    if keys.pressed(KeyCode::W) {
+        player_transform.translation.y += 2.;
+    }
+    if keys.pressed(KeyCode::A) {
+        player_transform.translation.x -= 2.;
+    }
+    if keys.pressed(KeyCode::S) {
+        player_transform.translation.y -= 2.;
+    }
+    if keys.pressed(KeyCode::D) {
+        player_transform.translation.x += 2.;
+    }
 }
